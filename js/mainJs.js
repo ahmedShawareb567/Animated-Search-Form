c7f0dd0fabd36817c90d0b093dba0fc57c3fae37
unknown <ahmedshawareb567@gmail.com>
2019-12-26 08:54:08 -0800

    last Modified

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,6 +1,12 @@
 
 
 let clickEl = document.getElementById('clickEl');
+
 clickEl.addEventListener('click', () => {
     clickEl.parentElement.classList.toggle('changeW');
+    clickEl.previousElementSibling.focus();
+});
+
+clickEl.previousElementSibling.addEventListener('blur', () => {
+    clickEl.parentElement.classList.remove('changeW');
 });
